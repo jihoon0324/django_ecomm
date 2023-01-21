4818f5d12c539c29f23c95e54f60bd0f7374230e
Jihoon Oh <jihoon.oh@edu.sait.ca>
2023-01-20 21:38:09 -0700

    add basket url and temp

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -19,7 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("store.urls" , namespace='store'))
+    path('basket/', include("basket.urls", namespace='basket')),
+    path('', include("store.urls" , namespace='store')),
+
+
+
 ]
 
 
